ac25f963fe3e245fdfa32c00e63c25c4608d1c7f
unknown <j.mcafee@ufl.edu>
2024-10-23 13:17:30 -0400

    updated program

--- Lab06.py
+++ Lab06.py
@@ -8,3 +8,28 @@ def encode(password):
         encrypted = encrypted + str(temp)
 
     return encrypted
+
+if __name__ == "__main__":
+
+    while(1):
+        print("       Menu\n    __________\n\n1. encode password\n2. decode password\n3. exit")
+        choice = input("\nenter menu option: ")
+
+        if choice == "1":
+            user_input = input("enter password: ")
+
+            new_password = encode(user_input)
+
+            print(f"Encoded password is: {new_password}")
+
+        elif choice == "2":
+
+            pass
+
+        elif choice == "3":
+
+            break
+
+        else:
+
+            print("not a valid menu option")
